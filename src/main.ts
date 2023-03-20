4be8d8e6d4c531269d20ecf439a13cb14b5e092d
Dev Giri <devgiri0082@gmail.com>
2023-03-20 21:20:10 +0545

    fix files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,4 @@
 import './style.css'
-import setupCanvas from './canvas/test'
-import drawBarChart from './canvas/barchat'
-import drawPieChart from './canvas/piechart'
-import advanceDrawingAndEvents from './canvas/chapter3'
-import animation from './canvas/chapter4'
-import drawBall from './canvas/ball'
-import snowAnimation from './canvas/showAnimation'
 import CanvasAnimation from './canvas/game'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html */`
